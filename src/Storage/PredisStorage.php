718c6b69a0b16048ec9009f3c052960e98a8548b
Benedict Etzel <developer@beheh.de>
2015-03-08 00:47:10 +0100

    implement PredisStorage

--- src/Storage/PredisStorage.php
+++ src/Storage/PredisStorage.php
@@ -7,4 +7,33 @@ use Predis\Client;
 
 class PredisStorage implements StorageInterface {
 
+	/**
+	 * @var Client;
+	 */
+	protected $client;
+
+	public function __construct(Client $client) {
+		$this->client = $client;
+	}
+
+	public function setValue($key, $value) {
+		$this->client->set($key, $value);
+	}
+
+	public function getValue($key) {
+		return $this->client->get($key, $value);
+	}
+
+	public function setTimestamp($key, $timestamp) {
+		$this->client->set($key.':time', $timestamp);
+	}
+
+	public function getTimestamp($key) {
+		$this->client->get($key.':time', $timestamp);
+	}
+
+	public function expire($key, $timestamp) {
+		return $this->client->expireat($key, $timestamp) === 1;
+	}
+
 }
