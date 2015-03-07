6f393ef78b648a6b3c2ff353d8edaad15f2ac747
Benedict Etzel <developer@beheh.de>
2015-03-07 11:12:21 +0100

    remove empty tests

--- test/FlapTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BehEh\Flaps;
-
-use PHPUnit_Framework_TestCase;
-
-class FlapTest extends PHPUnit_Framework_TestCase {
-
-}
--- test/FlapsTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BehEh\Flaps;
-
-use PHPUnit_Framework_TestCase;
-
-class FlapsTest extends PHPUnit_Framework_TestCase {
-
-}
