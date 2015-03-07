9c8b662e1d84356d97a98a76b4172e4aa4b8b1d0
Benedict Etzel <developer@beheh.de>
2015-03-07 11:42:53 +0100

    add test cases to LeakyBucketStrategyTest

--- tests/Throttling/LeakyBucketStrategyTest.php
+++ tests/Throttling/LeakyBucketStrategyTest.php
@@ -8,10 +8,18 @@ class LeakyBucketStrategyTest extends \PHPUnit_Framework_TestCase {
 	 * @covers BehEh\Flaps\Throttling\LeakyBucketStrategy::parseTime
 	 */
 	public function testParseTime() {
+		$this->assertEquals(0, LeakyBucketStrategy::parseTime(0));
+		$this->assertEquals(0, LeakyBucketStrategy::parseTime('0'));
+		$this->assertEquals(0, LeakyBucketStrategy::parseTime('0s'));
+		$this->assertEquals(1, LeakyBucketStrategy::parseTime(1));
+		$this->assertEquals(1, LeakyBucketStrategy::parseTime('1'));
 		$this->assertEquals(1, LeakyBucketStrategy::parseTime('1s'));
 		$this->assertEquals(2, LeakyBucketStrategy::parseTime('2s'));
 		$this->assertEquals(60, LeakyBucketStrategy::parseTime('1m'));
 		$this->assertEquals(90, LeakyBucketStrategy::parseTime('1.5m'));
+		$this->assertNull(LeakyBucketStrategy::parseTime('invalid'));
+		$this->assertNull(LeakyBucketStrategy::parseTime('1 m'));
+		$this->assertNull(LeakyBucketStrategy::parseTime('1k'));
 	}
 
 }
