3c8d586eb17e5a0173a966f43ba3456c7700c9fd
Benedict Etzel <developer@beheh.de>
2015-03-08 12:20:33 +0100

    improve key prefixing in PredisStorage

--- src/Storage/PredisStorage.php
+++ src/Storage/PredisStorage.php
@@ -16,25 +16,35 @@ class PredisStorage implements StorageInterface {
 		$this->client = $client;
 	}
 
+	private function prefixKey($key) {
+		return 'flaps:'.$key;
+	}
+
+	private function prefixTimestamp($timestamp) {
+		return $this->prefixKey('timestamp:'.$timestamp);
+	}
+
 	public function setValue($key, $value) {
-		$this->client->set($key, intval($value));
+		$this->client->set($this->prefixKey($key), intval($value));
 	}
 
 	public function getValue($key) {
-		return intval($this->client->get($key));
+		return intval($this->client->get($this->prefixKey($key)));
 	}
 
 	public function setTimestamp($key, $timestamp) {
-		$this->client->set($key.':time', floatval($timestamp));
+		$this->client->set($this->prefixTimestamp($key), floatval($timestamp));
 	}
 
 	public function getTimestamp($key) {
-		return floatval($this->client->get($key.':time'));
+		return floatval($this->client->get($this->prefixTimestamp($key)));
 	}
 
 	public function expireIn($key, $seconds) {
 		$redisTime = $this->client->time();
-		return $this->client->expireat($key, ceil($redisTime[0] + $seconds)) === 1;
+		$at = ceil($redisTime[0] + $seconds);
+		$this->client->expireat($this->prefixTimestamp($key), $at);
+		return $this->client->expireat($this->prefixKey($key), $at) === 1;
 	}
 
 }
