4e53daaa7e3582c1c7048d0151900a66da22edfe
Benedict Etzel <developer@beheh.de>
2015-03-08 01:33:15 +0100

    implement DoctrineCacheAdapter

--- src/Storage/DoctrineCacheAdapter.php
+++ src/Storage/DoctrineCacheAdapter.php
@@ -3,6 +3,43 @@
 namespace BehEh\Flaps\Storage;
 
 use BehEh\Flaps\StorageInterface;
+use Doctrine\Common\Cache\Cache;
 
 class DoctrineCacheAdapter implements StorageInterface {
+
+	/**
+	 * @var Cache;
+	 */
+	protected $cache;
+
+	public function __construct(Cache $cache) {
+		$this->cache = $cache;
+	}
+
+	public function setValue($key, $value) {
+		$this->cache->save($key, intval($value));
+	}
+
+	public function getValue($key) {
+		if(!$this->cache->contains($key)) {
+			return 0;
+		}
+		return intval($this->cache->fetch($key));
+	}
+
+	public function setTimestamp($key, $timestamp) {
+		$this->cache->save($key.':time', floatval($timestamp));
+	}
+
+	public function getTimestamp($key) {
+		if(!$this->cache->contains($key.':time')) {
+			return 0;
+		}
+		return intval($this->cache->fetch($key.':time'));
+	}
+
+	public function expireIn($key, $seconds) {
+		return false;
+	}
+
 }
